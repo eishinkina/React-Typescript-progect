b5371b40962bed2e6c0a4bb7092cbca235eddbc5
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-07-30 14:41:12 +0400

    rendered users and todos on the page using Rest API

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Card, { CardVariant } from "./components/Card";
-import UserList from "./components/UserList";
-import { IUser } from "./components/types/Types";
+import List from "./components/List";
+import { ITodo, IUser } from "./components/types/Types";
+import axios from "axios";
+import UserItem from "./components/UserItem";
+import TodoItem from "./components/TodoItem";
 
 const App = () => {
-  const[users, setUsers] = useState<IUser[]>([])
+  const [users, setUsers] = useState<IUser[]>([]);
+  const [todos, setTodos] = useState<ITodo[]>([]);
+  useEffect(() => {
+    fetchUsers();
+    fetchTodos();
+  }, []);
+  async function fetchUsers() {
+    try {
+      const response = await axios.get<IUser[]>(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      setUsers(response.data);
+    } catch (e) {
+      alert(e);
+    }
+  }
+  async function fetchTodos() {
+    try {
+      const response = await axios.get<ITodo[]>(
+        'https://jsonplaceholder.typicode.com/todos?_limit=10'
+      );
+      setTodos(response.data);
+    } catch (e) {
+      alert(e);
+    }
+  }
   return (
     <div>
       <Card
@@ -15,7 +43,14 @@ const App = () => {
       >
         <button>кнопка</button>
       </Card>
-      <UserList users={users} />
+      <List
+        items={users}
+        renderItem={(user: IUser) => <UserItem user={user} key={user.id} />}
+      />
+      <List
+        items={todos}
+        renderItem={(todo: ITodo) => <TodoItem todo={todo} key={todo.id} />}
+      />
     </div>
   );
 };
--- src/components/types/Types.tsx
+++ src/components/types/Types.tsx
@@ -1,14 +1,18 @@
-
 export interface IAddress {
-    street: string;
-    city: string; 
-    zipcode: string;
+  street: string;
+  city: string;
+  zipcode: string;
 }
 
 export interface IUser {
-    id: number;
-    name: string;
-    email: string;
-    address: IAddress
+  id: number;
+  name: string;
+  email: string;
+  address: IAddress;
+}
 
+export interface ITodo {
+  id: number;
+  title: string;
+  completed: boolean;
 }
